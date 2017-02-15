0ab32429693d2ddca77a78dfcde8f20ee3c62816
trinityproject <tebehrends@gmail.com>
2017-02-15 17:07:57 -0500

    Initial Commit

---
+++ DaySix.js
@@ -0,0 +1,142 @@
+/*
+Day Six
+Exercise One:
+------------------------------------
+*/
+var numberA = 2,
+var numberB = 2,
+var twoPlusTwo = addNumbers(2 + 2),
+
+function addNumbers(numberA, numberB) {
+  return(numberA + numberB);
+}
+console.log(twoPlusTwo)
+
+var numberA = 2,
+var numberB = 2,
+var twoPlusTwo = addNumbers(2,2),
+var twoPlusTwo = undefined,
+console.log(twoPlusTwo) // 4
+/*
+Exercise Two:
+------------------------------------
+
+var yell = "RED", "pink", "White", "Orange", "blue", "ultramarine"
+var cap = (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, R, S, T, U, V, W, X, Y, Z)
+
+function yell {
+  if (yell[0] == cap) {
+    return = true
+  }
+  console.log(true.logout)
+  console.log(yell[0].logout)
+  }
+  return(name[0] == cap)
+  console.log(name[0].logout)
+
+  re
+  console.log(name[0].remain)
+}
+  var capital = (i = [0]),
+
+for (var i = 0; i < names.length; i++)
+
+var i = 0;
+var x = 0;
+while (i < 5) {
+  i++;
+  if (i == 9 {
+    continue;
+
+  }
+  n += i;
+}
+var yell10;
+
+for (yell = 0; yell <9; yell ++) {
+
+}
+function yell10 {
+  return loop(function yell + [9])
+  break [(function yell >= loop*9)]
+}
+
+Exercise Three:
+-----------------------------------------
+var one = "eye", "ear", "feet", "elbow", "knee", "belly"
+var two = "five", "six", "seven", "eight", "nine"
+var three = "tree", "sand", "snow"
+
+function longest {
+  return(length);
+}
+console.log(names.one.length)
+console.log(names.two.length)
+console.log(names.three.length)
+
+Exercise Four:
+----------------------------------------
+var vow = a, A, e, E, i, I, o, O, u, U, y, Y
+
+function isVowel {
+  console.log(names[0] == vow)
+   then true
+  else
+   then false
+}
+console.log(isVowel)
+
+Exercise Five:
+----------------------------------------
+*/
+var myAge = 45;
+
+function myAge {
+  if (myAge < 100) {
+    ("False")
+ } else {
+    ("True")
+  }
+}
+console.log(myAge);
+
+/*
+Exercise Six:
+--------------------------------------
+
+var firstName = "Julia";
+var lastName = "Roberts";
+var fullName = firstName + lastname;
+
+console.log(fullName);
+
+CORRECTION:
+
+var firstName = "Julia";
+var lastName = "Roberts";
+
+function fullName {
+  return firstName + " " + lastName;
+}
+console.log(fullName);
+
+var yearBorn = 1975;
+var currentYear = 2016;
+Var age = currentYear - yearBorn;
+
+console.log(age); // 41
+
+CORRECTION:
+
+var yearBorn = 1975;
+var currentYear = 2016;
+var age = currentYear - yearBorn;
+
+function age {
+  return (currentYear - yearBorn)
+}
+
+console.log(age); // 41
+
+
+*/
